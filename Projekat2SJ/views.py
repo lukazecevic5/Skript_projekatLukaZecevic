a7b985410f84d7845691c25f48b2ffe523a9e48f
Luka Zecevic <lukazecevic@Lukas-MacBook-Air.local>
2020-12-21 18:09:12 +0100

    views initialized

--- Projekat2SJ/views.py
+++ Projekat2SJ/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
-
+from django import template
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Homepage!")
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
